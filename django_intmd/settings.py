e727f0ecd79b4017e11ad814dd388961ce79b8d2
kenta_mitsui <kentam12@gmail.com>
2025-05-13 19:36:50 +0900

    add: Djangoプロジェクトを作成、起動を確認

--- django_intmd/settings.py
+++ django_intmd/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wgq&ac9nf3le%rq=7ua4^p7c$i@w6)s-a9*ib+l^$zt2qc*-$p'
+SECRET_KEY = 'django-insecure-(qpz1$1rwm%ualvv&qjj3k)6groliz1x9^&7b(yop(aqw9cfw-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'matching_app.apps.MatchingAppConfig',
 ]
 
 MIDDLEWARE = [
