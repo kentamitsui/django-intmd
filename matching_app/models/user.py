73470388368f0b0b403360be10180e4718a0e161
kenta_mitsui <kentam12@gmail.com>
2025-05-25 00:07:46 +0900

    マイグレートまで完了

--- matching_app/models/user.py
+++ matching_app/models/user.py
@@ -6,6 +6,7 @@ from django.dispatch import receiver
 
 from matching_app.pkg.times import get_age_from_date_of_birth
 
+
 class UserManager(BaseUserManager):
     def create_user(
         self,
@@ -57,20 +58,36 @@ class UserManager(BaseUserManager):
         return user
 
 class User(AbstractUser):
-  
-@receiver(post_save, sender=User)
-def create_OneToOnes(instance, created, **kwargs):
-    if created:
-        from matching_app.models import UserProfile, UserVerification
-
-        age = get_age_from_date_of_birth(instance.date_of_birth)
-        UserProfile.objects.create(user=instance, age=age)
-        UserVerification.objects.create(user=instance)
     username = models.CharField(max_length=50, unique=False)
     email = models.EmailField(unique=True)
     date_of_birth = models.DateField(blank=False, null=False)
     icon = models.ImageField(upload_to="user_icons/", blank=True, null=True)
 
+    groups = models.ManyToManyField(
+        "auth.Group",
+        verbose_name="groups",
+        blank=True,
+        help_text="The groups this user belongs to. A user will get all permissions granted to each of their groups.",
+        related_name="matching_app_users",
+    )
+    user_permissions = models.ManyToManyField(
+        "auth.Permission",
+        verbose_name="user permissions",
+        blank=True,
+        help_text="Specific permissions for this user.",
+        related_name="matching_app_users",
+    )
+
     objects = UserManager()
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username", "date_of_birth"]
+    
+@receiver(post_save, sender=User)
+def create_OneToOnes(instance, created, **kwargs):
+    if created:
+        from matching_app.models.user_profile import UserProfile
+        from matching_app.models.user_verification import UserVerification
+
+        age = get_age_from_date_of_birth(instance.date_of_birth)
+        UserProfile.objects.create(user=instance, age=age)
+        UserVerification.objects.create(user=instance)
