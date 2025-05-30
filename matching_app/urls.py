bb6419a860165750147c8accbe72827d8fc27cf1
kenta_mitsui <kentam12@gmail.com>
2025-05-31 00:01:59 +0900

    セクション5-7まで完了

--- matching_app/urls.py
+++ matching_app/urls.py
@@ -3,11 +3,16 @@ from django.conf.urls.static import static
 from django.urls import path
 from matching_app.views.signup import signup
 from matching_app.views.user_profile import user_home
+from matching_app.views.index import index
+from matching_app.views.login import login_view, logout_view
 
 urlpatterns = (
     [
         path("signup/", signup, name="signup"),
         path("home/", user_home, name="user_home"),
+        path("", index, name="index"),
+        path("login/", login_view, name="login"),
+        path("logout/", logout_view, name="logout")
     ]
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
