98f3bfb2e2bbcc1bc34c679a32548406bc1ebdc0
kenta_mitsui <kentam12@gmail.com>
2025-06-02 15:49:42 +0900

    セクション5-10まで完了

--- matching_app/urls.py
+++ matching_app/urls.py
@@ -5,6 +5,7 @@ from matching_app.views.signup import signup
 from matching_app.views.user_profile import user_home
 from matching_app.views.index import index
 from matching_app.views.login import login_view, logout_view
+from matching_app.views.verify import verify_email, send_new_verification_code
 
 urlpatterns = (
     [
@@ -12,7 +13,9 @@ urlpatterns = (
         path("home/", user_home, name="user_home"),
         path("", index, name="index"),
         path("login/", login_view, name="login"),
-        path("logout/", logout_view, name="logout")
+        path("logout/", logout_view, name="logout"),
+        path("signup/verify/<int:id>/", verify_email, name="verify_email"), # 追加
+        path("signup/verify/resend/<int:id>/", send_new_verification_code, name="send_new_verification_code"), # 追加
     ]
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
