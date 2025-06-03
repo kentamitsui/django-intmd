dd8663a0afbf0319471d8a43c6a79abbc941ace1
kenta_mitsui <kentam12@gmail.com>
2025-06-03 18:00:41 +0900

    セクション6-7まで完了
    テストコードを実装

--- matching_app/views/user_profile.py
+++ matching_app/views/user_profile.py
@@ -1,13 +1,13 @@
 import random
 
 import structlog
+from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views.decorators.http import require_http_methods
 
 from matching_app.forms.user_profile import UserForm, UserProfileForm
-from django.contrib.auth import get_user_model
 
 logger = structlog.get_logger(__name__)
 
@@ -51,6 +51,7 @@ def user_profile_update(request: HttpRequest) -> HttpResponse:
         },
     )
 
+
 @login_required
 @require_http_methods(["GET"])
 def user_profile_list(request: HttpRequest) -> HttpResponse:
@@ -61,3 +62,10 @@ def user_profile_list(request: HttpRequest) -> HttpResponse:
         "user_profile_list.html",
         {"users": users},
     )
+
+
+@login_required
+@require_http_methods(["GET"])
+def user_profile_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    user = get_object_or_404(get_user_model().objects.select_related("userprofile"), pk=pk)
+    return render(request, "user_profile_detail.html", {"user": user})
