2d900edb097350f43f04982b46bbe29062470f0f
kenta_mitsui <kentam12@gmail.com>
2025-06-05 13:57:50 +0900

    セクション7-6まで完了
    募集投稿ページで新規作成が可能な事を確認

--- matching_app/urls.py
+++ matching_app/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 
 from matching_app.views.index import index
 from matching_app.views.login import login_view, logout_view
-from matching_app.views.recruitment import recruitment_timeline
+from matching_app.views.recruitment import recruitment_timeline, recruitment_detail, recruitment_create
 from matching_app.views.signup import signup
 from matching_app.views.user_profile import user_home, user_profile_detail, user_profile_list, user_profile_update
 from matching_app.views.verify import send_new_verification_code, verify_email
@@ -22,6 +22,8 @@ urlpatterns = (
         path("profiles/list/", user_profile_list, name="user_profile_list"),
         path("profiles/<int:pk>/", user_profile_detail, name="user_profile_detail"),
         path("recruitments/", recruitment_timeline, name="recruitment_timeline"),
+        path("recruitments/<int:pk>/", recruitment_detail, name="recruitment_detail"),
+        path("recruitment/create/", recruitment_create, name="recruitment_create"),
     ]
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
