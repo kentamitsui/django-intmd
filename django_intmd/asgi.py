d51d067ea84a483d8ad2ee0bf6163aa51b19ba6e
kenta_mitsui <kentam12@gmail.com>
2025-06-20 14:31:27 +0900

    ASGI設定を更新し、Djangoアプリケーションの初期化を整理。websocketルーティングを含むProtocolTypeRouterの構成を修正。

--- django_intmd/asgi.py
+++ django_intmd/asgi.py
@@ -1,30 +1,19 @@
-"""
-ASGI config for django_intmd project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
 import os
-import django
 
-# Djangoの設定を最初に読み込む
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_intmd.settings.base")
-django.setup()
-
+from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-from matching_app.channels.routing import websocket_urlpatterns
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_intmd.settings.base")
 
 django_asgi_app = get_asgi_application()
 
+from matching_app.channels.routing import websocket_urlpatterns
+
 application = ProtocolTypeRouter(
-  {
-    "http": django_asgi_app,
-    "websocket": AllowedHostsOriginValidator(AuthMiddlewareStack(URLRouter(websocket_urlpatterns))),
-  }
+    {
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(AuthMiddlewareStack(URLRouter(websocket_urlpatterns))),
+    }
 )
